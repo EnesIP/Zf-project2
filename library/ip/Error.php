e9f577ad18500b68faf36923592b7c942fcca3fd
Administrateur <Administrateur@WIN-N95DN42PNFI>
2014-07-08 10:56:45 +0200

    Class Error

---
+++ library/ip/Error.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class Error
+{
+	public function handleError($errno, $errstr, $errfile, $errline, $context)
+	{
+		echo 'Erreur de l\'application';
+	}
+	
+	
+	public function handleException(Exception $e)
+	{
+		echo 'Erreur de l\'application';
+	}
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,20 +1,21 @@
 <?php
 define ( 'DS', DIRECTORY_SEPARATOR );
 define ( 'APP_ENV', getenv ( 'APPLICATION_ENV' ) ?  : 'production' );
-define ( 'ROOT_PATH', dirname ( dirname ( __DIR__ ) ) );
+define ( 'ROOT_PATH', dirname ( dirname ( __DIR__ ) ) ); //Répertoire de l'application
 define ( 'SRC_PATH', ROOT_PATH . DS . 'src' );
 define ( 'PUBLIC_PATH', SRC_PATH . DS . 'public' );
 define ( 'VENDOR_PATH', ROOT_PATH . DS . 'vendor' );
 define ( 'APP_PATH', SRC_PATH . DS . 'application' );
+define ( 'LIB_PATH', ROOT_PATH . DS . 'library' );
 require_once VENDOR_PATH . DS . 'autoload.php';
-
+require_once LIB_PATH . DS . '/ip/Error.php';
 $autoloader = Zend_Loader_Autoloader::getInstance ();
 
 if ("development" === APP_ENV) {
 \php_error\reportErrors ();
 } else {
-	set_exception_handler ( "exceptionHandler" );
-	set_error_handler ( "errorHandler" );
+	set_exception_handler (array("Error", "handleException")); //Un tableau avec le nom de la classe et le nom de la méthode en paramètre
+	set_error_handler (array("Error", "handleError" ));
 }
 
 // try{
@@ -24,16 +25,5 @@ $application->run ();
 /*
  * }catch (Zend_Config_Exception $e){ // toujours utiliser le typage objet echo "Problème de config " . $e->getMessage(); if ('development' === APP_ENV){ echo "<pre>" . $e->getTraceAsString() . "</pre>"; } }catch (Zend_Application_Exception $e){ // toujours utiliser le typage objet echo "Problème de l'application " .$e->getMessage(); if ('development' === APP_ENV){ echo "<pre>" . $e->getTraceAsString() . "</pre>"; } }catch (Exception $e){ // toujours utiliser le typage objet echo "Problème général" . $e->getMessage(); if ('development' === APP_ENV){ echo "<pre>" . $e->getTraceAsString() . "</pre>"; } }
  */
-function exceptionHandler($e) // Permet de catcher les erreurs en général
-{
-	echo "Erreur de l'application";
-	// Mettre dans les logs
-	// Envoyer un mail
-}
-function errorHandler($errno, $errstr, $errfile, $errline, $context) // Permet de catcher les erreurs php
-{
-	echo "Erreur de l'application";
-	// Mettre dans les logs
-	// Envoyer un mail
-}
+
 
